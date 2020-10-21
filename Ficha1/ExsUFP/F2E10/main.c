212a82fb2c4fa55224f58d28d17bf2458f1fcdd5
Maria Beatriz <41204@ufp.edu.pt>
2020-10-21 21:24:49 +0100

    f2e10

--- Ficha1/ExsUFP/F2E10/main.c
+++ Ficha1/ExsUFP/F2E10/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    int n = 0, i;
+    printf("Insere um numero entre 1 e 10 para apresentar a sua tabuada: ");
+    scanf("%d", &n);
+    while(n>10 || n<0){
+        scanf("%d", &n);
+    }
+
+    for(i = 1; i <= 10; i++){
+        printf("\n%d * %d = %d", n, i, i * n);
+    }
 }
